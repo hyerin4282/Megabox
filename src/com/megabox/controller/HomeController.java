0a0cbfe560eabf174b280d3542bf4590428480b4
hyerin4282 <48782290+hyerin4282@users.noreply.github.com>
2019-05-31 11:43:04 +0900

    123

--- src/com/megabox/controller/HomeController.java
+++ src/com/megabox/controller/HomeController.java
@@ -38,7 +38,7 @@ public class HomeController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub1213
 		doGet(request, response);
 	}
 
